07b52cc07c4225b72fd14b991208828af331b178
Po-Chih Huang <brianhuang1019@gmail.com>
2018-01-08 03:40:04 +0800

    Update feature_extracter.py

--- python/feature_extracter.py
+++ python/feature_extracter.py
@@ -113,33 +113,6 @@ pickle.dump(conversations, open('data/conversations_lenmax22_formersents2_with_f
 # pickle.dump(former_sents, open('data/conversations_lenmax22_former_sents', 'wb'), True)
 print("Time Elapsed: {} secs\n".format(time.time() - ts))
 
-# # some statistics of training data
-# max_a = -1
-# max_b = -1
-# max_a_ind = -1
-# max_b_ind = -1
-# sum_a = 0.
-# sum_b = 0.
-# len_a_list = []
-# len_b_list = []
-# for i in range(len(conversations)):
-#     len_a = len(conversations[i][0])
-#     len_b = len(conversations[i][1].split())
-#     if len_a > max_a:
-#         max_a = len_a
-#         max_a_ind = i
-#     if len_b > max_b:
-#         max_b = len_b
-#         max_b_ind = i
-#     sum_a += len_a
-#     sum_b += len_b
-#     len_a_list.append(len_a)
-#     len_b_list.append(len_b)
-# np.save("data/lenmax22_former-sents2_a_list", np.array(len_a_list))
-# np.save("data/lenmax22_former-sents2_b_list", np.array(len_b_list))
-# print("max_a_ind {}, max_b_ind {}".format(max_a_ind, max_b_ind))
-# print("max_a {}, max_b {}, avg_a {}, avg_b {}".format(max_a, max_b, sum_a/len(conversations), sum_b/len(conversations)))
-
 ts = time.time()
 conversations = []
 # former_sents = []
@@ -171,33 +144,6 @@ for conversation in raw_movie_conversations:
 pickle.dump(conversations, open('data/conversations_lenmax22_former_sents2', 'wb'), True)
 print("Time Elapsed: {} secs\n".format(time.time() - ts))
 
-# some statistics of training data
-# max_a = -1
-# max_b = -1
-# max_a_ind = -1
-# max_b_ind = -1
-# sum_a = 0.
-# sum_b = 0.
-# len_a_list = []
-# len_b_list = []
-# for i in range(len(conversations)):
-#     len_a = len(conversations[i][0])
-#     len_b = len(conversations[i][1].split())
-#     if len_a > max_a:
-#         max_a = len_a
-#         max_a_ind = i
-#     if len_b > max_b:
-#         max_b = len_b
-#         max_b_ind = i
-#     sum_a += len_a
-#     sum_b += len_b
-#     len_a_list.append(len_a)
-#     len_b_list.append(len_b)
-# np.save("data/lenmax22_former-sents2_a_list", np.array(len_a_list))
-# np.save("data/lenmax22_former-sents2_b_list", np.array(len_b_list))
-# print("max_a_ind {}, max_b_ind {}".format(max_a_ind, max_b_ind))
-# print("max_a {}, max_b {}, avg_a {}, avg_b {}".format(max_a, max_b, sum_a/len(conversations), sum_b/len(conversations)))
-
 ts = time.time()
 conversations = []
 print('len conversation', len(raw_movie_conversations))
@@ -223,30 +169,3 @@ for conversation in raw_movie_conversations:
         print('con_count {}, traindata_count {}'.format(con_count, traindata_count))
 pickle.dump(conversations, open('data/conversations_lenmax22', 'wb'), True)
 print("Time Elapsed: {} secs\n".format(time.time() - ts))
-
-# # some statistics of training data
-# max_a = -1
-# max_b = -1
-# max_a_ind = -1
-# max_b_ind = -1
-# sum_a = 0.
-# sum_b = 0.
-# len_a_list = []
-# len_b_list = []
-# for i in range(len(conversations)):
-#     len_a = len(conversations[i][0])
-#     len_b = len(conversations[i][1].split())
-#     if len_a > max_a:
-#         max_a = len_a
-#         max_a_ind = i
-#     if len_b > max_b:
-#         max_b = len_b
-#         max_b_ind = i
-#     sum_a += len_a
-#     sum_b += len_b
-#     len_a_list.append(len_a)
-#     len_b_list.append(len_b)
-# np.save("data/lenmax22_a_list", np.array(len_a_list))
-# np.save("data/lenmax22_b_list", np.array(len_b_list))
-# print("max_a_ind {}, max_b_ind {}".format(max_a_ind, max_b_ind))
-# print("max_a {}, max_b {}, avg_a {}, avg_b {}".format(max_a, max_b, sum_a/len(conversations), sum_b/len(conversations)))
